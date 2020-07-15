cf37aacf61f1835e78bd3dcd2f6843342dd122e9
ayeminwai <ayeminwai@gmail.com>
2020-07-15 18:44:17 +0630

    Third commit change string util

--- src/main/java/com/amw/java/springbootcicd/utility/StringUtil.java
+++ src/main/java/com/amw/java/springbootcicd/utility/StringUtil.java
@@ -1,7 +1,13 @@
 package com.amw.java.springbootcicd.utility;
 
 public class StringUtil {
+	public static boolean isNull(String str) {
+		return str.isEmpty() || "".equals(str) || str == null;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("String Util starting");
+		
+		isNull("");
 	}
 }
