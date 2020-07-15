af73585bbad86e384d5c2bf91085777df132fd0d
ayeminwai <ayeminwai@gmail.com>
2020-07-15 18:29:11 +0630

    first-commit

---
+++ src/test/java/com/amw/java/springbootcicd/SpringBootCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amw.java.springbootcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
